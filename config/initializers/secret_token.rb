f5e959ab6fa602a4e4d3ebff03fedfa4be0fc20f
Andrew Gordon <ame.gordon@altanisgroup.com>
2012-08-09 15:54:53 -0400

    New Rails application Test - Added Rails Status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Altanisbook::Application.config.secret_token = 'cfb79b22c451c4894f59deccbace88ce0da2cf2fbb7d7be80b2827b9573fe4144b6b4addc10b01cf51e5396af560ed6a8081b6ac505d82522a9b2572a4fbcbe0'
